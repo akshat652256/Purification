4c00dc57ee54fdfda0af1fc83096ee69de172c87
Akshat Satyanarayan <pkmnakshat@gmail.com>
2025-08-14 12:55:24 +0530

    fix

--- fl-purification/src/models/Defensive_models/AE.py
+++ fl-purification/src/models/Defensive_models/AE.py
@@ -1,38 +1,43 @@
-import torchvision.transforms.functional as F
+import torch
 import torch.nn as nn
-class SimpleAutoencoder(nn.Module):
+
+class DetectorIReformer(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 3, stride=2, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(32, 64, 3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64,128, 3, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Flatten(),
-            nn.Linear(128*4*4, 512),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(512, 128*4*4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Unflatten(1, (128, 4, 4)),
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1, output_padding=0), # 4x4 → 8x8
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1, output_padding=0), # 8x8 → 14x14
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.ConvTranspose2d(32, 3, 4, stride=2, padding=1, output_padding=0), # 14x14 → 28x28
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),  # Conv.Sigmoid 3x3x3
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2),                # AveragePooling 2x2
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),  # Conv.Sigmoid 3x3x3
             nn.Sigmoid()
         )
-    
+        self.decoder = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),  # Conv.Sigmoid 3x3x3
+            nn.Sigmoid(),
+            nn.Upsample(scale_factor=2),                # Upsampling 2x2
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),  # Conv.Sigmoid 3x3x3
+            nn.Sigmoid(),
+            nn.Conv2d(1, 1, kernel_size=(3,3), padding=1) # Conv.Sigmoid 3x3x1
+        )
     def forward(self, x):
-        z = self.encoder(x)
-        out = self.decoder(z)
-        out = F.center_crop(out, [28, 28])
-        return out
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+class DetectorII(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),   # Conv.Sigmoid 3x3x3
+            nn.Sigmoid(),
+            nn.Conv2d(1, 1, kernel_size=3, padding=1),   # Conv.Sigmoid 3x3x3
+            nn.Sigmoid()
+        )
+        self.decoder = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=(3,3), padding=1) # Conv.Sigmoid 3x3x1
+        )
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
